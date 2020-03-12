eb083649835edbc44581a15f6731c6a9d5d6b47e
leonardo.prodrigue1 <leopr.95@hotmail.com>
2020-03-12 11:56:38 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/appopps/Convite_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.appopps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Convite_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.convite_layout);
+    }
+}
---
+++ app/src/main/java/br/com/sp/senac/appopps/Incorreta_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.appopps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Incorreta_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.incorreta_layout);
+    }
+}
